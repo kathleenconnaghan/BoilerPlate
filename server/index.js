2f6e231814d756aba33b934ab4e8a20047681a4d
kathleenconnaghan <77138134+kathleenconnaghan@users.noreply.github.com>
2021-07-26 12:35:57 -0400

    BoilerPlate: completed express, api routes (generic) and 404/500 error handling

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path =require('path')
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// ------ Logging Middleware: server logs for debugging
+app.use(morgan('dev'));
+
+//--------- Static Middleware: for requests for static assets from server (.js, .css, imgs)
+app.use(express.static(path.join(__dirname, './path/to/static/assets')));
+
+//------- Body Parser: middleware to parse the body in the req.body 
+// depreciated !!!!!!!!!!!!!!! check this later
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//----------- matches all requests to /api
+app.use('/api', require('./apiRoutes'));
+
+// ------------- SPA
+// GOES AFTER ALL ROUTES : Send index.html for any other requests
+app.get('*', function(req, res){
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  }) 
+
+// ---------- Log out 500 errors
+app.use(function(err, req, res, next) {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal Server Error!');
+})
